fefac5ab3fcae736d4b89ebf8f2c8e3669f77ff8
LEE HYUN WOO <lhwda1388@naver.com>
2020-03-06 23:10:53 +0900

    # WARNING: head commit changed in the meantime
    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # authServerSample
+
+jks 생성
+
+keytool -genkeypair -alias Key의 별칭
+                    -keyalg RSA(key암호화 알고리즘)
+                    -keypass Key 암호
+                    -keystore 저장될 key의 파일명 
+                    -storepass 저장될 파일의 암호
+                    
+#key 깨질경우    
+keytool -genkeypair -alias samplejks -keyalg RSA -validity 3650 -keypass samplejkspwd -keystore sampleauth.jks -storepass storepwd
+
+# 공개키 정보 확인
+keytool -list -rfc --keystore sampleauth.jks | openssl x509 -inform pem -pubkey
--- src/main/java/com/sample/authserver/config/AuthServerConfig.java
+++ src/main/java/com/sample/authserver/config/AuthServerConfig.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import com.sample.authserver.service.CustomUserDetailService;
 
@@ -66,14 +69,25 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter{
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		// jwt
-		endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+		endpoints.accessTokenConverter(accessTokenConverter()).userDetailsService(userDetailService);
 	}
 	
+	/*
 	@Bean
-	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+	public JwtAccessTokenConverter accessTokenConverter() {
 		log.debug("signKey {}", signKey);
 		JwtAccessTokenConverter convter = new JwtAccessTokenConverter();
 		convter.setSigningKey(signKey);
+		return convter;
+	}
+	*/
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("sampleauth.jks"), "storepwd".toCharArray());
+		JwtAccessTokenConverter convter = new JwtAccessTokenConverter();
+		convter.setKeyPair(keyStoreKeyFactory.getKeyPair("samplejks"));
+		
 		return convter;
 	}
 }
